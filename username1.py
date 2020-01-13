ef1012a841936b95c48b058d4cd43d0ef94b7cff
Nobushige <ynobu2@yahoo.co.jp>
2020-01-13 09:55:40 +0900

    First commit

---
+++ username1.py
@@ -0,0 +1,23 @@
+class UserName:
+    def __init__(self, name):
+        if not (4 <= len(name) <= 20):
+            print(len(name))
+            raise ValueError(f'{name}は文字数のルール違反です')
+
+        self.name = name
+
+    def screen_name(self):
+        return self.name.upper()
+
+
+hibiki = UserName(name='hibiki')
+
+# print(hibiki)
+# print(type(hibiki))
+# print(hibiki.name)
+
+print(hibiki.screen_name())
+
+# UserNameクラスのインスタンス化
+# sho = UserName(name = 'sho')
+# print(sho.name)
